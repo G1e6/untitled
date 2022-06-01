654f25c116e4dfec009f631d80beb5690e0b1b3d
G1e6 <gleb-potapov@rambler.ru>
2022-06-01 19:29:19 +0500

    изменение кода теста

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -12,6 +12,14 @@ public class BonusServiceTest {
     }
 
     @Test
+    public void testShouldCalculateForNoRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        long expected = 10;
+        long actual = service.calculate(1000, false);
+        //System.out.println(bonus);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
     public void testShouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
         long expected = 500;
@@ -20,5 +28,18 @@ public class BonusServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testShouldCalculateForNoRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        long expected = 500;
+        long actual = service.calculate(1000000, false);
+        //System.out.println(bonus);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
 
 }
